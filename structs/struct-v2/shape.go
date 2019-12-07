21005376422f4ca7e3bb0c6bec5414c33405d084
ahsan-info <ahsan.ror@gmail.com>
2019-12-07 12:32:50 +0200

    added area calculation ot v2

---
+++ structs/struct-v2/shape.go
@@ -0,0 +1,11 @@
+package main
+
+// Perimeter calculation
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+// Area calculation
+func Area(width, height float64) float64 {
+	return width * height
+}
